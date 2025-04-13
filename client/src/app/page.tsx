07e9c90a07b7cc6eaaea65ab25654376ed454056
NucleusHQ <hi@nucleushq.io>
2025-04-13 18:12:29 +0530

    Frontend cleaned up

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,31 +6,59 @@ import { type ChatOutput } from "@/types";
 import { useState } from "react";
 
 export default function Home() {
-  const [outputs, setOutputs] = useState<ChatOutput[]>([]);
-  const [isGenerating, setIsGenerating] = useState(false);
+  const [currentQuery, setCurrentQuery] = useState("");
+  const [history, setHistory] = useState([])
+
+  const handleSubmit = async (e: React.FormEvent, query: string) => {
+    e.preventDefault()
+    setCurrentQuery("");
+
+    try {
+      const response = await fetch(`http://localhost:8000/invoke`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: query,
+          thread_id: "234"
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error");
+      }
+
+      const data = await response.json()
+      const { answer } = data || {};
+
+
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  };
 
   return (
     <div
-      className={`container pt-10 pb-32 min-h-screen ${
-        outputs.length === 0 && "flex items-center justify-center"
-      }`}
+      className={`container pt-10 pb-32 min-h-screen ${history.length === 0 && "flex items-center justify-center"
+        }`}
     >
       <div className="w-full">
-        {outputs.length === 0 && (
+        {history.length === 0 && (
           <h1 className="text-4xl text-center mb-5">
-            What do you want to know?
+            Ask me a Question!
           </h1>
         )}
 
         <TextArea
-          setIsGenerating={setIsGenerating}
-          isGenerating={isGenerating}
-          outputs={outputs}
-          setOutputs={setOutputs}
+          onSubmit={handleSubmit}
+          currentQuery={currentQuery}
+          setCurrentQuery={setCurrentQuery}
         />
 
-        {outputs.map((output, i) => {
-          return <Output key={i} output={output} />;
+        {history.map((item, i) => {
+          return <Output key={i} output={item} />;
         })}
       </div>
     </div>
